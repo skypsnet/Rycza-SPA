9d6c3096924b9ed9e69d661c033e39d2557c1788
Ricardo Balam <ricardo.chagoya@vinneren.com.mx>
2026-03-31 11:29:20 -0600

    Delete another entry point

--- src/app.js
+++
@@ -1,8 +0,0 @@
-
-import { createApp } from "vue";
-import VueSplide from '@splidejs/vue-splide';
-import App from "./App.vue";
-import '@splidejs/vue-splide/css/sea-green';
-import router from "./router";
-import store from "./store";
-createApp(App).use(store).use(router).use( VueSplide ).mount("#app");
